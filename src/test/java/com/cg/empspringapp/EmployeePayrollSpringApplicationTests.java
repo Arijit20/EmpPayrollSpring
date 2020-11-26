40f91f88d5bdddf935bb2f94fd4d7f9b31c5b34c
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-11-27 00:43:50 +0530

    [Arijit]Added : Spring Project Configuration

---
+++ src/test/java/com/cg/empspringapp/EmployeePayrollSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.empspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

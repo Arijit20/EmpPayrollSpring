21ff93d8dfc73de6db9060d831c02710af13513a
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-11-28 20:05:55 +0530

    [Arijit]Refactored : Name Validation Added

--- src/main/java/com/cg/empspringapp/dto/User.java
+++ src/main/java/com/cg/empspringapp/dto/User.java
@@ -1,5 +1,8 @@
 package com.cg.empspringapp.dto;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.cg.empspringapp.model.EmpPayroll;
 
 import lombok.Data;
@@ -19,6 +22,14 @@ public class User {
 	        this.id = employee.getId();
 	        this.name = employee.getName();
 	        this.salary = employee.getSalary();
+	    }
+	    
+	    public void setName(String name) {
+	    	Pattern pattern = Pattern.compile("^[A-Z]{1}[a-zA-Z\\s]{2,}$");
+			Matcher matcher = pattern.matcher(name);
+			Boolean b = matcher.find();
+			if(b) this.name = name;
+			else this.name = null;
 	    }
 
 }
